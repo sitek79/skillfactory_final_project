f94ec64cb420c9ab668c80f88d560237061d4e04
sitek79 <atikhonov79@gmail.com>
2022-09-24 14:46:15 +0300

    24 Sept 2022. Before Refactoring

---
+++ src/main/java/hm/sitek/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package hm.sitek.repository;
+
+import hm.sitek.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
