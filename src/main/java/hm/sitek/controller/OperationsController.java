d4d027a435ce89a0663fd1b02e0722b2e3a972d1
sitek79 <atikhonov79@gmail.com>
2022-10-09 19:15:32 +0300

    Stage_2. 9 Okt 2022. Operations List. Add REST API v1

---
+++ src/main/java/hm/sitek/controller/OperationsController.java
@@ -0,0 +1,23 @@
+package hm.sitek.controller;
+
+import hm.sitek.model.Operations;
+import hm.sitek.repository.OperationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/terminal/v1")
+public class OperationsController {
+
+    @Autowired
+    OperationsRepository operationsRepository;
+
+    @GetMapping("/operations")
+    public List<Operations> getAllOperations() {
+        return operationsRepository.findAll();
+    }
+}
